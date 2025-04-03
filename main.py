57653717f92579061aabccd929b495f0d3b5b046
StarkySeb <sebastian.stary@student.vitkovickastredni.cz>
2025-04-03 12:27:05 +0200

    playwright kodiky

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from playwright.sync_api import sync_playwright
+
+def test_page_title():
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/index.html"))
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(f"file://{file_path}")
+        title = page.title()
+        print(title)
+        browser.close()
+
+
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("file://" + "index.html")  # Načte lokální HTML soubor
+    print(page.title())  # Vypíše titulek stránky
+    browser.close()
