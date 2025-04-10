ac5a0421cde7f6800461a56ee399b0e23cfcf290
StarkySeb <sebastian.stary@student.vitkovickastredni.cz>
2025-04-10 08:18:31 +0200

    jupii rozjelo se mi to konecne

--- tests/test_locator.py
+++ tests/test_locator.py
@@ -1,17 +1,18 @@
 import os
-from playwright.sync_api import sync_playwright, expect
 import urllib.parse
+from playwright.sync_api import sync_playwright, expect
 
 def test_page_title():
+
     cesta = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/index.html"))
-    cesta = urllib.parse.unquote(cesta)
+    cesta = urllib.parse.unquote(cesta) 
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=False)  
         page = browser.new_page()
         page.goto(f"file://{cesta}")
-
-        nadpis_1 = page.locator('h1').first() 
-        expect(nadpis_1).to_be_visible()  
+        page.wait_for_load_state("load")  
+        nadpis = page.locator("h1").first
+        expect(nadpis).to_be_visible()
 
         browser.close()
